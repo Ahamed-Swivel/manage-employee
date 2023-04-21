ca9d8ea510004faffe7251d43a007fd04ef8583f
Ahamed Rasheed <ahamed@swivelgroup.com.au>
2023-04-21 07:34:27 +0530

    feat: unit test updated

--- __test__/components/index.test.tsx
+++ __test__/components/index.test.tsx
@@ -6,7 +6,7 @@ import Employee from '@/models/Employee'
 import EmployeeList from '@/components/EmployeeList'
 import ManageEmployee from '@/components/ManageEmployee'
 
-const dummyEmployee = [{
+const dummyEmployee: Employee[] = [{
   id: "644127d23e6e56a2b8e8b91d",
   firstName: "Henri",
   lastName: "Rodriguez",
@@ -18,10 +18,14 @@ const dummyEmployee = [{
 
 describe('Components', () => {
   it('renders EmployeeCard', () => {
+    const onDelete = (value: Employee) => {
+      expect(value.id).toBe(dummyEmployee[0].id)
+    }
+
     const component = render(
       <EmployeeCard
         employees={dummyEmployee}
-        onDelete={(employee: Employee) => {employee}}
+        onDelete={(employee: Employee) => {onDelete(employee)}}
       />
     )
 
@@ -29,10 +33,14 @@ describe('Components', () => {
   })
 
   it('renders EmployeeList', () => {
+    const onDelete = (value: Employee) => {
+      expect(value.id).toBe(dummyEmployee[0].id)
+    }
+
     const component = render(
       <EmployeeList
         employees={dummyEmployee}
-        onDelete={(employee: Employee) => {employee}}
+        onDelete={(employee: Employee) => {onDelete(employee)}}
       />
     )
 
@@ -40,10 +48,14 @@ describe('Components', () => {
   })
 
   it('renders Manage Employee - Update Employee', () => {
+    const onSubmit = (value: Employee) => {
+      expect(value.id).toBe(dummyEmployee[0].id)
+    }
+
     const component = render(
       <ManageEmployee
         employee={dummyEmployee[0]}
-        onSubmit={(employee: Employee) => {employee}}
+        onSubmit={(employee: Employee) => {onSubmit(employee)}}
       />
     )
 
@@ -51,9 +63,13 @@ describe('Components', () => {
   })
 
   it('renders Manage Employee - Add new Employee', () => {
+    const onSubmit = (value: Employee) => {
+      expect(value.id).toBe(dummyEmployee[0].id)
+    }
+
     const component = render(
       <ManageEmployee
-        onSubmit={(employee: Employee) => {employee}}
+        onSubmit={(employee: Employee) => {onSubmit(employee)}}
       />
     )
 
